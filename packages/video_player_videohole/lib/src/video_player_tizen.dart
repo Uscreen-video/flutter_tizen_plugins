9089cb1b0832dfc9f0466d10b05b86f668aad670
Ivan Kryak <kryak.iv@gmail.com>
2025-07-16 17:40:51 +0200

    Update video_player_tizen.dart

--- packages/video_player_videohole/lib/src/video_player_tizen.dart
+++ packages/video_player_videohole/lib/src/video_player_tizen.dart
@@ -8,7 +8,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-import 'package:video_player_platform_interface/video_player_platform_interface.dart';
+import 'package:video_player_platform_interface/video_player_platform_interface.dart' hide AudioTrack;
 import 'messages.g.dart';
 import 'tracks.dart';
 
